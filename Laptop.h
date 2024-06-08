98459815942e95d0a2f7ac0368052605c0d1ae90
purple-leopard <97757699+purple-leopard@users.noreply.github.com>
2024-06-08 21:54:38 +0000

    Refactor Laptop

---
+++ Laptop.h
@@ -0,0 +1,46 @@
+#pragma once
+#include <iostream>
+#include "LeakedObjectDetector.h"
+
+/*
+ copied UDT 2:
+ */
+struct Laptop
+{
+    Laptop();
+    ~Laptop();
+    std::string brand = "Apple";
+    std::string model;
+    std::string operatingSystemVersion = "Snow Leopard";
+    int memoryCapacity = 16;
+    int numberOfProcessorCores = 4;
+    
+    struct Battery
+    {
+        Battery();
+        ~Battery();
+        std::string type = "Li-ion";
+        float capacity;
+        int chargeCycles = 1000;
+        float maxChargeCurrent = 2.0f;
+        float voltage = 18.0f;
+
+        bool charge(const float chargeToLevel = 100.0f);
+        float checkCapacityRemaining(const std::string& fuelGuageAlgorithm = "ModelGauge");
+        void limitChargeCurrent(float inputCurrent, const float temperatureLimit = 85.8f);
+        void drain();
+        void printChargeCapacity() const;
+
+        JUCE_LEAK_DETECTOR(Battery)
+    };
+
+    void replaceBattery(const Battery& newBattery);
+    bool launchProgram(const std::string& programName);
+    bool invokeCompiler() const;
+    void cycleCharge(Battery& battery, const int numCycles);
+    void printBrand() const;
+
+    Battery currentBattery;
+
+    JUCE_LEAK_DETECTOR(Laptop)
+};
--- main.cpp
+++ main.cpp
@@ -70,6 +70,7 @@ void Axe::aConstMemberFunction() const { }
 #include <iostream>
 #include "LeakedObjectDetector.h"
 #include "Dog.h"
+#include "Laptop.h"
 
 struct DogWrapper
 {
@@ -93,49 +94,6 @@ struct DogCollarWrapper
     }
 
     Dog::DogCollar* dogCollarPtr = nullptr;
-};
-
-/*
- copied UDT 2:
- */
-struct Laptop
-{
-    Laptop();
-    ~Laptop();
-    std::string brand = "Apple";
-    std::string model;
-    std::string operatingSystemVersion = "Snow Leopard";
-    int memoryCapacity = 16;
-    int numberOfProcessorCores = 4;
-    
-    struct Battery
-    {
-        Battery();
-        ~Battery();
-        std::string type = "Li-ion";
-        float capacity;
-        int chargeCycles = 1000;
-        float maxChargeCurrent = 2.0f;
-        float voltage = 18.0f;
-
-        bool charge(const float chargeToLevel = 100.0f);
-        float checkCapacityRemaining(const std::string& fuelGuageAlgorithm = "ModelGauge");
-        void limitChargeCurrent(float inputCurrent, const float temperatureLimit = 85.8f);
-        void drain();
-        void printChargeCapacity() const;
-
-        JUCE_LEAK_DETECTOR(Battery)
-    };
-
-    void replaceBattery(const Battery& newBattery);
-    bool launchProgram(const std::string& programName);
-    bool invokeCompiler() const;
-    void cycleCharge(Battery& battery, const int numCycles);
-    void printBrand() const;
-
-    Battery currentBattery;
-
-    JUCE_LEAK_DETECTOR(Laptop)
 };
 
 struct LaptopWrapper
@@ -162,96 +120,6 @@ struct LaptopBatteryWrapper
     Laptop::Battery* laptopBatteryPtr = nullptr;
 };
 
-Laptop::Laptop() : model("Macbook")
-{
-    std::cout << "Laptop being constructed\n";
-}
-
-Laptop::~Laptop()
-{
-    std::cout << "Laptop being destructed\n";
-}
-
-Laptop::Battery::Battery() : capacity(1000.0f)
-{
-    std::cout << "Battery being constructed\n";
-}
-
-Laptop::Battery::~Battery()
-{
-    std::cout << "Battery being destructed\n";
-}
-
-bool Laptop::Battery::charge(const float chargeToLevel) 
-{
-    float chargeLevel = 75.0f;
-    std::cout << "battery charging to level: " << chargeToLevel << "%\n";
-    return chargeLevel >= chargeToLevel;
-}
-
-float Laptop::Battery::checkCapacityRemaining(const std::string& fuelGuageAlgorithm) 
-{
-    float capacityRemaining = 1000.0f;
-    std::cout << "checking battery capacity remaining out of max capacity " << capacity << " mAh, using " << fuelGuageAlgorithm << "\n";
-    return capacityRemaining;
-}
-
-void Laptop::Battery::limitChargeCurrent(float inputCurrent, const float temperatureLimit) 
-{
-    float temperature = 38.0f;
-    if (inputCurrent > maxChargeCurrent || temperature > temperatureLimit) 
-    {
-        inputCurrent = maxChargeCurrent;
-        std::cout << "charge current limited to " << maxChargeCurrent << "\n";
-    }
-}
-
-void Laptop::Battery::drain()
-{
-    while (capacity > 200.0f)
-    {
-        capacity -= 200.0f;
-        std::cout << "battery capacity now at " << capacity << " mAh\n";
-    }
-    std::cout << "battery low, please recharge\n";
-}
-
-void Laptop::Battery::printChargeCapacity() const
-{
-    std::cout << "laptop's battery's charge capacity: " << this->capacity << " mAh\n";
-}
-
-void Laptop::replaceBattery(const Battery& newBattery) 
-{
-    currentBattery = newBattery;
-    std::cout << model <<" battery replaced with type: " << newBattery.type << "\n";
-}
-
-bool Laptop::launchProgram(const std::string& programName) 
-{
-    std::cout << "launching program: " << programName << "\n";
-    return true;
-}
-
-bool Laptop::invokeCompiler() const
-{
-    std::cout << "compiler invoked" << "\n";
-    return true;
-}
-
-void Laptop::cycleCharge(Battery& battery, const int numCycles)
-{
-    for(int i = 0; i < numCycles; ++i)
-    {
-        ++battery.chargeCycles;
-        std::cout << "cycling battery charge, cycles are now at: " << battery.chargeCycles << "\n";
-    }
-}
-
-void Laptop::printBrand() const
-{
-    std::cout << "laptop brand: " << this->brand << "\n";
-}
 /*
  copied UDT 3:
  */
